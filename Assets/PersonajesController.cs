3074c1678e9a0422dfc61e4b023882fcedbba299
Maria Fernanda Flores <mafer_271099@live.com>
2018-06-01 23:45:05 -0500

    Cambiando tu pinshe archivo, ajio.

---
+++ Assets/PersonajesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajesController : MonoBehaviour {
+
+    public string nombreCompleto;
+    public int edad;
+    public string físico;
+
+    public PersonajesController(string nombreCompleto, int edad, string físico)
+    {
+        this.nombreCompleto = nombreCompleto;
+        this.edad = edad;
+        this.físico= físico;
+    }
+
+    public PersonajesController()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Programa.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Programa : MonoBehaviour
+{
+
+    public Transform target;
+
+    public GameObject personaje1, personaje2, personaje3, personaje4;
+    GameObject imagenBorrar;
+
+    public Text info;
+
+    List<PersonajesController> personaje = new List<PersonajesController>();
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Bienvenido, se te presentarán 4 personajes, presiona en el botón para ver más información.");
+        personaje.Add(new PersonajesController("Juana Del Arco", 27, "a pesar de ser algo flacucha, es muy buena usando el arco y flecha."));
+        personaje.Add(new PersonajesController("Garen Spinner", 22, "en físico es robusto y grande, experto con la espada."));
+        personaje.Add(new PersonajesController("Havana Banana", 30, "alguna vez fue una celestial, ahora utiliza las estrellas para ayudar a sus aliados."));
+        personaje.Add(new PersonajesController("Khada Jhin", 24, "es un meticuloso criminal que ve el asesinato como arte, a través de su rifle."));
+
+        //
+
+        imagenBorrar = GameObject.FindGameObjectWithTag("PersonajeActual");
+    }
+
+    public void Button_Click(GameObject objeto)
+    {
+        Instantiate(objeto, target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        imagenBorrar = GameObject.FindGameObjectWithTag("PersonajeActual");
+
+        if (target.transform.childCount > 1)
+        {
+            Destroy(imagenBorrar);
+        }
+    }
+
+    public void Button_Info()
+    {
+
+    }
+
+}
